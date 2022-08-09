a6ce0a6dfd18caa542a8b245c4b03db46a3440c1
Annika Hannig <annika@hannig.cc>
2022-08-09 14:38:18 +0200

    renamed project

--- src/env.rs
+++ src/env.rs
@@ -3,20 +3,20 @@
 // github_token returns the github access token
 // from the environment.
 pub fn github_token() -> String {
-    std::env::var("NOLF_GITHUB_TOKEN")
+    std::env::var("MAUNZ_GITHUB_TOKEN")
         .expect("GITHUB_TOKEN env variable is required")
 }
 
 // state_file returns the path to the state file
 // from the environment.
 pub fn state_file() -> String {
-   std::env::var("NOLF_STATE")
-       .expect("NOLF_STATE should contain a path to the state file")
+   std::env::var("MAUNZ_STATE")
+       .expect("MAUNZ_STATE should contain a path to the state file")
 }
 
 // issues_path returns the path to all issues
 pub fn issues_path() -> String {
-    std::env::var("NOLF_ISSUES")
-        .expect("NOLF_ISSUES should contain the path to the issues")
+    std::env::var("MAUNZ_ISSUES")
+        .expect("MAUNZ_ISSUES should contain the path to the issues")
 }
 
