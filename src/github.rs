7a1c924beb2e325ee11c82608aa3d4012f40e856
Annika Hannig <annika@hannig.cc>
2022-08-10 19:59:30 +0000

    create and fetch issue

--- src/github.rs
+++ src/github.rs
@@ -1,44 +1,48 @@
 
 use octocrab::{
     Octocrab,
-    params,
     models,
 };
 
-use crate::env;
+use crate::issue;
 
-pub async fn fetch_issues() -> Result<Vec<models::issues::Issue>, octocrab::Error> {
-    let octocrab = Octocrab::builder()
-        .personal_token(env::github_token())
-        .build()?;
+pub struct Github {
+    token: String,
+    org: String,
+    project: String,
+}
 
-    let mut page = octocrab
-        .issues("annikahannig", "life")
-        .list()
-        .state(params::State::All)
-        .per_page(100)
-        .send()
-        .await?;
+impl Github {
 
-    let mut issues: Vec<models::issues::Issue> = Vec::new();
-
-    // Get from all pages
-    loop {
-        for issue in &page {
-            // Skip any issues that are PRs.
-            if issue.pull_request.is_some() {
-                continue;
-            }
-            issues.push(issue.clone());
-        }
-        page = match octocrab
-            .get_page::<models::issues::Issue>(&page.next)
-            .await?
-        {
-            Some(next_page) => next_page,
-            None => break,
+    pub fn new(token: String, org: String, project: String) -> Github {
+        Github{
+            token: token,
+            org: org,
+            project: project,
         }
     }
 
-    Ok(issues)
+    // create issue will create a new github issue
+    pub async fn create_issue(&self, issue: &issue::Issue) -> Result<models::issues::Issue, octocrab::Error> {
+        let octocrab = Octocrab::builder()
+            .personal_token(self.token.clone())
+            .build()?;
+        octocrab
+            .issues(self.org.clone(), self.project.clone())
+            .create(&issue.meta.title)
+            .body(&issue.text)
+            .send()
+            .await
+    }
+
+    // fetch a single issue by ID
+    pub async fn fetch_issue(&self, id: u64) -> Result<models::issues::Issue, octocrab::Error> {
+        let octocrab = Octocrab::builder()
+            .personal_token(self.token.clone())
+            .build()?;
+        octocrab
+            .issues(self.org.clone(), self.project.clone())
+            .get(id)
+            .await
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,33 @@ mod issue;
 mod state;
 mod github;
 
+
+
 #[tokio::main]
 async fn main() -> Result<(), octocrab::Error> {
     let mut state = state::from_file(env::state_file()).unwrap();
     let repo = issue::from_path(env::issues_path()).unwrap();
 
-    // Fetch all issues from github
-    // let github_issues = github::fetch_issues().await?;
+    let gh = github::Github::new(
+        env::github_token(),
+        String::from("annikahannig"),
+        String::from("life"),
+    );
 
     // Sync state with repo
     for (id, issue) in repo {
         let mut issue_state = state.track_issue(&id);
-        // issue_state.mark_open(93109)
-    }
-
-    /*
-    let issues = github::fetch_issues().await?;
-
-    for issue in issues {
-        println!("{}: {}, ({})", issue.number, issue.title, issue.state);
-        match issue.body {
-            Some(body) =>
-                println!("{}", body),
-            _ => ()
+        match issue_state.github_id {
+            None => {
+                // issue_state.github_id = gh.create_issue(&issue).await?;
+                let res = gh.create_issue(&issue).await?;
+            },
+            Some(id) => {
+                let github_issue = gh.fetch_issue(id).await?;
+                println!("check state and check if needs reopen{}", id)
+            },
         }
     }
-    */
 
     // Write state
     state.save(env::state_file()).unwrap();
