f1d17b244429a6025770dcfecc3512bc632f89ee
Annika Hannig <annika@hannig.cc>
2022-08-15 17:20:45 +0000

    update state

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,17 @@ async fn main() -> Result<(), octocrab::Error> {
         match issue_state.github_id {
             None => {
                 // issue_state.github_id = gh.create_issue(&issue).await?;
-                let res = gh.create_issue(&issue).await?;
+                let github_issue = gh.create_issue(&issue).await?;
+                issue_state.mark_open(github_issue.id.into_inner());
             },
             Some(id) => {
                 let github_issue = gh.fetch_issue(id).await?;
+                if github_issue.state == String::from("open") {
+                    issue_state.is_open = true
+                } else {
+                    issue_state.is_open = false
+                }
+
                 println!("check state and check if needs reopen{}", id)
             },
         }
